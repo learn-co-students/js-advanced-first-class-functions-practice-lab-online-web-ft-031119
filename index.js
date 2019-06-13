9d9daa8e5414e04fa134155e04f93602e46453f9
Daniel Schneider <daschne8@gmail.com>
2019-06-13 00:16:55 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Code your solution in this file!
+function logDriverNames(drivers){
+  drivers.forEach(function(driver){console.log(driver.name)})
+}
+function logDriversByHometown(drivers,hometown){
+  logDriverNames(drivers.filter(function(driver){return driver.hometown === hometown}))
+}
+function driversByRevenue(drivers){
+  drivers.sort(function(driver){return driver.revenue})
+}
