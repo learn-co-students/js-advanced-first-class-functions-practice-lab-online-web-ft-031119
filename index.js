55552c7b7299903d081e8a556f1c987755c4c5ab
Katie Wilday <wildayk@gmail.com>
2019-07-06 10:27:03 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution in this file!
+function logDriverNames(drivers){
+  drivers.forEach(function(driver){
+    console.log(driver.name);
+  });
+}
+
+function logDriversByHometown(drivers, query){
+  drivers.forEach(function(driver){
+    if (driver.hometown === query){
+      console.log(driver.name);
+    }
+  });
+}
+
+function driversByRevenue(drivers) {
+  return drivers.slice().sort(function(driverOne, driverTwo){
+    return driverOne.revenue - driverTwo.revenue;
+  });
+}
+
+function driversByName(drivers) {
+  return drivers.slice().sort(function(driverOne, driverTwo){
+    return driverOne.name.localeCompare(driverTwo.name);
+  });
+}
+
+function totalRevenue(drivers) {
+  return drivers.reduce(function(total, current){
+    return current.revenue + total;
+  }, 0);
+}
+
+function averageRevenue(drivers) {
+   return totalRevenue(drivers) / drivers.length;
+}
